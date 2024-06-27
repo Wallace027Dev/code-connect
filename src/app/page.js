de68a475e4c8cbe31a4713fd3d6d49c07c429fa9
wallacevieira <wallaceofc@hotmail.com>
2024-06-27 17:22:44 -0300

    feat: paginate posts

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,35 @@
 import { CardPost } from "@/components/CardPost";
-import styles from './page.module.css'
+import styles from "./page.module.css";
 import logger from "@/logger";
+import Link from "next/link";
 
-async function getAllPosts() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
-  if (!response.ok) {
-    logger.error("Alguma coisa errada aconteceu");
+async function getAllPosts(page) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/posts?_page=${page}&_per_page=4`
+    );
+    if (!response.ok) throw new Error("Falha na rede");
+    return response.json();
+  } catch (error) {
+    logger.error("Ops, algo correu mal: " + error.message);
     return [];
   }
-  logger.info("Posts obtidos com sucesso");
-  return response.json();
 }
 
-export default async function Home() {
-  const posts = await getAllPosts();
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1;
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
 
   return (
     <main className={styles.main}>
       {posts.map((post) => (
-        <CardPost post={post} />
+        <CardPost key={post.id} post={post} />
       ))}
+
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
   );
 }
